9bb924ac76caa68016c2814af76c4950e5f8972c
abreuof <abreuof@gmail.com>
2020-09-18 01:18:32 +0000

    17/09 Primeiro model com relacionamentos

---
+++ back-end/models/SalaAula.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const esquema = mongoose.Schema({
+    nome: { type: String, required: true },
+    capacidade: { type: Number, default: 20 },
+    recursos_didaticos: { type: String}
+})
+
+/*
+    Parametros do metodo mongoose.model()
+    1 - nome do modelo (sempre igual a nome do arquivo)
+    2 - estrutura (esquema) do modelo
+    3 - nome da coleção (collection) em que os objetos criados a partir do modelo serão armazenados no mongoDB
+    */
+   module.exports = mongoose.model('SalaAula', esquema, 'salas_aula')
