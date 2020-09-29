1112b3694907652f49ae859014fe138ece301c25
abreuof <abreuof@gmail.com>
2020-09-29 00:57:04 +0000

    28/09 - Models

---
+++ back-end/models/Jogo.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const esquema = mongoose.Schema({
+    nome: { type: String, required: true },
+    preco: { type: Number, required: true },
+    plataforma: { type: String, required: true },
+    data: { type: Date, required: true }    
+})
+
+/*
+    Parametros do metodo mongoose.model()
+    1 - nome do modelo (sempre igual a nome do arquivo)
+    2 - estrutura (esquema) do modelo
+    3 - nome da coleção (collection) em que os objetos criados a partir do modelo serão armazenados no mongoDB
+*/
+   module.exports = mongoose.model('Jogo', esquema, 'jogos')
---
+++ back-end/models/Venda.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const esquema = mongoose.Schema({
+    data: { type: Date, required: true },  
+    vendedor: { type: String, required: true },
+    jogo: { type: mongoose.ObjectId, ref: 'Jogo' },
+    filme: { type: mongoose.ObjectId, ref: 'Filme' },
+    musica: { type: mongoose.ObjectId, ref: 'Musica' },
+    software: { type: mongoose.ObjectId, ref: 'Software' }      
+})
+
+/*
+    Parametros do metodo mongoose.model()
+    1 - nome do modelo (sempre igual a nome do arquivo)
+    2 - estrutura (esquema) do modelo
+    3 - nome da coleção (collection) em que os objetos criados a partir do modelo serão armazenados no mongoDB
+*/
+   module.exports = mongoose.model('Venda', esquema, 'vendas')
