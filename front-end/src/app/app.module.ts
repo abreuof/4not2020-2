c61b99347e145b43288d257eb4ea90f941d552d2
abreuof <abreuof@gmail.com>
2020-10-20 01:32:18 +0000

    19/10 - Front-end refeito

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,24 +1,25 @@
+import { MaterialModule } from './material/material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PrincipalComponent } from './ui/principal/principal.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderBarComponent } from './ui/header-bar/header-bar.component';
-import {​​ MaterialModule }​​ from './material/material.module';
+import { MainTollbarComponent } from './ui/main-tollbar/main-tollbar.component';
+import { MainToolbarComponent } from './ui/main-toolbar/main-toolbar.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    PrincipalComponent,
-    HeaderBarComponent
+    MainTollbarComponent,
+    MainToolbarComponent
   ],
   imports: [
     BrowserModule,
-    MaterialModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ front-end/src/app/ui/main-tollbar/main-tollbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-tollbar',
+  templateUrl: './main-tollbar.component.html',
+  styleUrls: ['./main-tollbar.component.scss']
+})
+export class MainTollbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
