ecb73603514735c9de8b505f134ef75c8ad9aba5
abreuof <abreuof@gmail.com>
2020-11-10 01:41:33 +0000

    09-11 Término do componente de listagem e início do comp. de formulário

--- front-end/src/app/curso/curso-list/curso-list.component.ts
+++ front-end/src/app/curso/curso-list/curso-list.component.ts
@@ -1,5 +1,6 @@
-import { CursoService } from './../curso.service';
 import { Component, OnInit } from '@angular/core';
+import { CursoService } from '../curso.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-curso-list',
@@ -11,20 +12,39 @@ export class CursoListComponent implements OnInit {
   // Nome da entidade no plural
   cursos : any = []
 
-  displayedColumns: string[] = ['nome', 'carga_horaria', 'nivel', 'valor_curso', 'editar', 'excluir']
+  // Quais colunas serão exibidas na tabela, e em qual ordem
+  displayedColumns: string[] = ['nome', 'carga_horaria', 'nivel', 'valor_curso', 'editar', 'excluir']   
 
-  // Injeção de dependencia ou inversão de controle
-  constructor(private cursoSrv : CursoService) { }
+  // Injeção de dependência ou inversão de controle
+  constructor(
+      private cursoSrv : CursoService,
+      private snackBar : MatSnackBar
+  ) { }
 
   async ngOnInit() {
     this.cursos = await this.cursoSrv.listar()
     console.log(this.cursos)
   }
 
-  excluir(id: string) {
-      if(confirm('Deseja realmente excluir?')) {
-          alert('Vai excluir o registro com id=' + id)
+  async excluir(id: string) {
+    if(confirm('Deseja realmente excluir?')) {
+      try {
+        await this.cursoSrv.excluir(id)
+        // 1) Recarregar os dados da tabela
+        this.ngOnInit()
+        // 2) Dar feedback para o usuário com mensagem
+        this.snackBar.open('Item excluído com sucesso.', 'Entendi', {
+          duration: 5000 // 5 segundos
+        })
       }
+      catch(erro) {
+        // 3) Dar feedback de erro para o usuário
+        this.snackBar.open('ERRO: não foi possível excluir este item.', 'Que pena!', {
+          duration: 5000 // 5 segundos
+        })
+        console.log(erro)
+      }
+    }
   }
 
 }
