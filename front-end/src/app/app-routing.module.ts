f231b4d258f261f4e9a3c50cc0fd30f97ed2d0f3
abreuof <abreuof@gmail.com>
2020-11-17 01:40:18 +0000

    16/11 Início de listagem e form de entidade relacionada

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
+import { TurmaListComponent } from './turma/turma-list/turma-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CursoListComponent } from './curso/curso-list/curso-list.component';
 import { CursoFormComponent } from './curso/curso-form/curso-form.component';
 
 const routes: Routes = [
-    // Nomes de rota no Angular (path) não começam com uma barra
+    // Nomes de rota no Angular (path) NÃO começam com uma barra
     { path: 'curso', component: CursoListComponent },
     { path: 'curso/novo', component: CursoFormComponent },
-    { path: 'curso/:id', component: CursoFormComponent }
+    { path: 'curso/:id', component: CursoFormComponent },
+
+    { path: 'turma', component: TurmaListComponent }
 ];
 
 @NgModule({
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,14 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 // Habilitar formatação de moeda e data em português
-import {​​​​ registerLocaleData }​​​​ from '@angular/common';
+import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
 registerLocaleData(localePt);
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MainTollbarComponent } from './ui/main-tollbar/main-tollbar.component';
 import { MainToolbarComponent } from './ui/main-toolbar/main-toolbar.component';
 import { MainFooterComponent } from './ui/main-footer/main-footer.component';
 import { MainMenuComponent } from './ui/main-menu/main-menu.component';
@@ -18,17 +17,19 @@ import { HttpClientModule } from '@angular/common/http';
 import { CursoListComponent } from './curso/curso-list/curso-list.component';
 import { CursoFormComponent } from './curso/curso-form/curso-form.component';
 import { FormsModule } from '@angular/forms';
-
+import { TurmaListComponent } from './turma/turma-list/turma-list.component';
+import { TurmaFormComponent } from './turma/turma-form/turma-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainTollbarComponent,
     MainToolbarComponent,
     MainFooterComponent,
     MainMenuComponent,
     CursoListComponent,
-    CursoFormComponent
+    CursoFormComponent,
+    TurmaListComponent,
+    TurmaFormComponent
   ],
   imports: [
     BrowserModule,
